a25794172506d46dce4962983084c1885f31a19d
Aarron <aarronkerin@daviesturner.co.uk>
2018-02-21 15:25:53 +0000

    added stuff

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorRefresh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To My Calculator");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+            int a, b;
+            Console.WriteLine("Enter a number");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            b = int.Parse(Console.ReadLine());
+
+            int result = a * b;
+
+            Console.WriteLine("Multiplication is: " + result);
+            Console.ReadKey();
+
+            
+            
+            
+        }
+
+        
+        
+    }
+}
